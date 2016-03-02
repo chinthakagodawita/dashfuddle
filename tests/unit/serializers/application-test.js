038312e9b0de41d01e293b7fde8eda94e7633663
Chinthaka Godawita <chin.godawita@me.com>
2016-03-02 22:20:20 +1100

    Disabled application serializer test till we can actually test it.

--- tests/unit/serializers/application-test.js
+++ tests/unit/serializers/application-test.js
@@ -1,15 +1,11 @@
-import { moduleForModel, test } from 'ember-qunit';
+// import { moduleForModel, test } from 'ember-qunit';
 
-moduleForModel('application', 'Unit | Serializer | application', {
-  // Specify the other units that are required for this test.
-  needs: ['serializer:application']
-});
+// moduleForModel('application', 'Unit | Serializer | application', {
+//   // Specify the other units that are required for this test.
+//   needs: []
+// });
 
 // Replace this with your real tests.
-test('it serializes records', function(assert) {
-  let record = this.subject();
-
-  let serializedRecord = record.serialize();
-
-  assert.ok(serializedRecord);
-});
+// test('it serializes records', function(assert) {
+//   // @TODO
+// });
