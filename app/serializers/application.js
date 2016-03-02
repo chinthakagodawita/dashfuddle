5e3a96d847a32bbd43efd8ca1c42b0c9ead1c8cb
Chinthaka Godawita <chin.godawita@me.com>
2016-03-02 22:08:50 +1100

    Remove debug, disable npm progress.

--- app/serializers/application.js
+++ app/serializers/application.js
@@ -1,22 +1,16 @@
 import DS from 'ember-data';
 
-export default DS.RESTSerializer.extend(DS.EmbeddedRecordsMixin, {
-  normalizeArrayResponse (store, primaryModelClass, payload, id, requestType) {
+const { RESTSerializer, EmbeddedRecordsMixin } = DS;
+
+export default RESTSerializer.extend(EmbeddedRecordsMixin, {
+  normalizeArrayResponse(store, primaryModelClass, payload, id, requestType) {
     let newPayload = {};
 
     newPayload[primaryModelClass.modelName] = payload;
     newPayload[primaryModelClass.modelName] = newPayload[primaryModelClass.modelName].filter(function(item) {
       return !item.archived;
     });
-    console.log(payload[0]);
+    // console.log(payload[0]);
     return this._super(store, primaryModelClass, newPayload, id, requestType);
-  },
-  normalizeResponse (store, primaryModelClass, payload, id, requestType) {
-    // console.log('id', id);
-    return this._super(...arguments);
   }
-  // keyForRelationship(key, typeClass, method) {
-  //   console.warn('keyfor', ...arguments);
-  //   return this._super(...arguments);
-  // }
 });
