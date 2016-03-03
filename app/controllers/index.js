a470b36a711f4b6983fcffbae598d5851a4bd5b5
Chinthaka Godawita <chin.godawita@me.com>
2016-03-03 22:52:40 +1100

    Added project select.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -4,9 +4,13 @@ const { Controller, inject } = Ember;
 
 export default Controller.extend({
   session: inject.service('session'),
+  selectedProject: null,
   actions: {
     invalidateSession() {
       this.get('session').invalidate();
+    },
+    selectProject(project) {
+      this.set('selectedProject', project);
     }
   }
 });
